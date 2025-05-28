b2c307e929eb00467d213d0e17b373760a243e9a
Julia Vilela <125197027+Juliavilelas@users.noreply.github.com>
2025-05-28 18:49:57 -0300

    Create Triggers.sql

---
+++ Triggers.sql
@@ -0,0 +1,25 @@
+CREATE TRIGGER dbo.credits_earned
+ON takes
+AFTER INSERT, DELETE
+AS
+  
+BEGIN
+    IF EXISTS (SELECT * FROM inserted)
+    BEGIN
+        UPDATE s
+        SET s.tot_cred = s.tot_cred + c.credits
+        FROM dbo.student s
+        INNER JOIN inserted i ON s.ID = i.ID
+        INNER JOIN dbo.course c ON i.course_id = c.course_id;
+    END
+
+    IF EXISTS (SELECT * FROM deleted)
+    BEGIN
+        UPDATE s
+        SET s.tot_cred = s.tot_cred - c.credits
+        FROM dbo.student s
+        INNER JOIN deleted d ON s.ID = d.ID
+        INNER JOIN dbo.course c ON d.course_id = c.course_id;
+    END
+      
+END;
